4e903c7cb17adcef22636ce1a77a102e84ef50fa
rofleksey <evilmorty@yandex.ru>
2018-04-12 15:54:37 +0300

    Fixed spaces in controllers.js

--- background.js
+++ background.js
@@ -1,22 +1,26 @@
+	String.prototype.regexIndexOf = function(regex, startpos) {
+		var indexOf = this.substring(startpos || 0).search(regex);
+		return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+	}
 	chrome.webRequest.onBeforeRequest.addListener(function (r) {
 		if (r.url.indexOf("controllers.js")!=-1) {
 			var request = new XMLHttpRequest();
 			request.open('GET', r.url, false);  // `false` makes the request synchronous
 			request.send(null);
 			var text = request.responseText;
-			text = text.replace("isAllCorrect: isAllCorrect", "isAllCorrect: true");
-			if(text.indexOf("totalCount: totalCount") != -1) {
-				text = text.replace("correctCount: correctCount", "correctCount: totalCount")
-				.replace("correctCount: correctAns", "correctCount: totalCount")
-				.replace("correctCount: totalHorizontalCorrectAns", "correctCount: totalCount");
-			} else if(text.indexOf("totalCount: totalAns") != -1) {
-				text = text.replace("correctCount: correctCount", "correctCount: totalAns")
-				.replace("correctCount: correctAns", "correctCount: totalAns")
-				.replace("correctCount: totalHorizontalCorrectAns", "correctCount: totalAns");
-			} else if(text.indexOf("totalCount: totalHorizontalCorrectQuestion") != -1) {
-				text = text.replace("correctCount: correctCount", "correctCount: totalHorizontalCorrectQuestion")
-				.replace("correctCount: correctAns", "correctCount: totalHorizontalCorrectQuestion")
-				.replace("correctCount: totalHorizontalCorrectAns", "correctCount: totalHorizontalCorrectQuestion");
+			text = text.replace(/isAllCorrect:\s*isAllCorrect/, "isAllCorrect: true");
+			if(text.regexIndexOf(/totalCount:\s*totalCount/) != -1) {
+				text = text.replace(/correctCount:\s*correctCount/, "correctCount: totalCount")
+				.replace(/correctCount:\s*correctAns/, "correctCount: totalCount")
+				.replace(/correctCount:\s*totalHorizontalCorrectAns/, "correctCount: totalCount");
+			} else if(text.regexIndexOf(/totalCount:\s*totalAns/) != -1) {
+				text = text.replace(/correctCount:\s*correctCount/, "correctCount: totalAns")
+				.replace(/correctCount:\s*correctAns/, "correctCount: totalAns")
+				.replace(/correctCount:\s*totalHorizontalCorrectAns/, "correctCount: totalAns");
+			} else if(text.regexIndexOf(/totalCount:\s*totalHorizontalCorrectQuestion/) != -1) {
+				text = text.replace(/correctCount:\s*correctCount/, "correctCount: totalHorizontalCorrectQuestion")
+				.replace(/correctCount:\s*correctAns/, "correctCount: totalHorizontalCorrectQuestion")
+				.replace(/correctCount:\s*totalHorizontalCorrectAns/, "correctCount: totalHorizontalCorrectQuestion");
 			}
 			return { redirectUrl: "data:text/javascript,"  + encodeURIComponent(text) };
 		}
--- clicker.js
+++ clicker.js
@@ -9,14 +9,16 @@ var roflanClick = function(e) {
 	dispatchMouseEvent(e, 'click', true, true);
 	dispatchMouseEvent(e, 'mouseup', true, true);
 }
-var pudge = setInterval(function(){
-	try{
-		var el = document.querySelector('#content-iframe').contentWindow.document
-		.querySelector('#ScormContent').contentWindow.document
-		.querySelector('#sec > footer > div.div2 > nav > div.right-button-bar > div:nth-child(1) > button');
-		if(el) {
-			window.roflanClick(el);
-			clearInterval(pudge);
-		}
-	} catch(e){}
-}, 1000);
+setTimeout(function(){
+	var pudge = setInterval(function(){
+		try{
+			var el = document.querySelector('#content-iframe').contentWindow.document
+			.querySelector('#ScormContent').contentWindow.document
+			.querySelector('#sec > footer > div.div2 > nav > div.right-button-bar > div:nth-child(1) > button');
+			if(el) {
+				window.roflanClick(el);
+				clearInterval(pudge);
+			}
+		} catch(e){}
+	}, 1000);
+}, 4000);
